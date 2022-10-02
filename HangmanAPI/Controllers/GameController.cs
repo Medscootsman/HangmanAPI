2fb79a80e42cd2d74a8a621e6bc0fc82a34f5d78
Murrray <murraylyne2@gmail.com>
2022-10-02 16:52:20 +0100

    Add GameController

---
+++ HangmanAPI/Controllers/GameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangmanAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+    }
+}
