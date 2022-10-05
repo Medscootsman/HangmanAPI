53671b64291081c12e428c8557898da2ff459308
Murrray <murraylyne2@gmail.com>
2022-10-05 17:23:02 +0100

    Add get for getting a single word

---
+++ HangmanAPI/Controllers/WordController.cs
@@ -0,0 +1,30 @@
+﻿using HangmanAPI.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangmanAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordController : ControllerBase {
+        private IGameService gameService;
+        private IWordService wordService;
+        private IGuessService guessService;
+
+        public WordController(IGameService gameService, IGuessService guessService, IWordService wordService) {
+            this.gameService = gameService;
+            this.guessService = guessService;
+            this.wordService = wordService;
+        }
+
+        [HttpGet("/{id}")]
+        public async Task<IActionResult> GetWord(Guid id) {
+
+            var wordModel = await wordService.GetSingleWord(id);
+
+            if (wordModel == null) {
+                return NotFound();
+            }
+
+            return Ok(wordModel);
+        }
+    }
+}
