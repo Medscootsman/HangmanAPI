349dc961648267091c6262edae084576f6c6393f
Murrray <murraylyne2@gmail.com>
2022-10-06 18:11:20 +0100

    Update integration test to test Word CRUD operations

--- HangmanAPI.Test/IntegrationTest.cs
+++ HangmanAPI.Test/IntegrationTest.cs
@@ -4,21 +4,16 @@ using HangmanAPI;
 using Newtonsoft.Json;
 using HangmanAPI.Model.Entity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
 
 namespace HangmanAPI.Test {
     public class IntegrationTest {
-        private Guid gameId;
         private WebApplicationFactory<Program> application;
         private HttpClient client;
         JsonSerializer serializer;
         public IntegrationTest() {
-            var projectDir = AppDomain.CurrentDomain.BaseDirectory;
-            var configPath = Path.Combine(projectDir, "appsettings.json");
-            application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => {
-                builder.ConfigureAppConfiguration((context, conf) => {
-                    conf.AddJsonFile(configPath);
-                });
-            });
+            application = new WebApplicationFactory<Program>();
             client = application.CreateClient();
             serializer = new JsonSerializer();
         }
@@ -29,7 +24,7 @@ namespace HangmanAPI.Test {
             CheckContentType(response);
             Assert.NotNull(response);
 
-            Assert.True(response.IsSuccessStatusCode);
+            Assert.True(response.IsSuccessStatusCode, $"response code was actually {response.StatusCode}");
 
             using var streamReader = new StreamReader(await response.Content.ReadAsStreamAsync());
             using var jsonReader = new JsonTextReader(streamReader);
@@ -37,18 +32,14 @@ namespace HangmanAPI.Test {
             var content = serializer.Deserialize<Guid>(jsonReader);
 
             Assert.True(content != Guid.Empty);
-            gameId = content;
         }
 
         [Fact]
         public async Task CanGetSingleGameFromId() {
-            if (gameId == Guid.Empty) {
-                return;
-            }
-
+            var gameId = await getGameForTesting();
             var response = await client.GetAsync($"api/game/{gameId}");
             Assert.NotNull(response);
-            Assert.True(response.IsSuccessStatusCode);
+            Assert.True(response.IsSuccessStatusCode, $"response code was actually {response.StatusCode}");
             CheckContentType(response);
 
             var content = await DeserializeObject<GameExportModel>(response);
@@ -63,16 +54,14 @@ namespace HangmanAPI.Test {
 
         [Fact]
         public async Task CanMakeGuess() {
-            if (gameId == Guid.Empty) {
-                return;
-            }
+            var gameId = await getGameForTesting();
 
             const char letterToTest = 'a';
 
             var response = await client.PutAsync($"api/game/{gameId}/{letterToTest}", null);
 
             Assert.NotNull(response);
-            Assert.True(response.IsSuccessStatusCode);
+            Assert.True(response.IsSuccessStatusCode, $"response code was actually {response.StatusCode}");
             CheckContentType(response);
 
             var content = await DeserializeObject<GuessUpdateModel>(response);
@@ -81,11 +70,71 @@ namespace HangmanAPI.Test {
                 Assert.True(content.Word.Contains(letterToTest));
             }
         }
-
+        [Fact]
         public async Task ShouldNotBeAbleToSubmitStrings() {
+            var gameId = await getGameForTesting();
             var response = await client.PutAsync($"api/game/{gameId}/fergwer", null);
             Assert.NotNull(response);
+            Assert.True(!response.IsSuccessStatusCode, $"response code was actually {response.StatusCode}");
         }
+
+        [Fact]
+        public async Task CanGetAllWords() {
+            var response = await client.GetAsync("api/word/words");
+            Assert.NotNull(response);
+            Assert.True(response.IsSuccessStatusCode, $"response code was actually {response.StatusCode}");
+            CheckContentType(response);
+
+            var content = await DeserializeObject<List<WordModel>>(response);
+
+            Assert.NotNull(content);
+        }
+
+        [Fact]
+        public async Task TestWordCRUD() {
+            const string wordToCreate = "testword";
+            //Create
+            var response = await client.PostAsync($"api/word/{wordToCreate}", null);
+            Assert.NotNull(response);
+            Assert.True(response.IsSuccessStatusCode, $"response code was actually {response.StatusCode}");
+            CheckContentType(response);
+
+            var content = await DeserializeObject<WordModel>(response);
+
+            Assert.NotNull(content);
+            Assert.Equal(wordToCreate, content.WordString);
+
+            //Read
+            var readResponse = await client.GetAsync($"api/word/{content.WordId}");
+            Assert.NotNull(readResponse);
+            Assert.True(readResponse.IsSuccessStatusCode, $"response code was actually {readResponse.StatusCode}");
+            CheckContentType(response);
+
+            var readContent = await DeserializeObject<WordModel>(readResponse);
+
+            Assert.NotNull(readContent);
+
+            Assert.Equal(wordToCreate, readContent.WordString);
+
+            //Update
+            content.WordString = "testwordthatisupdated";
+
+            var updateResponse = await client.PutAsync("api/word", GetStringContent(content));
+            Assert.NotNull(updateResponse);
+            Assert.True(updateResponse.IsSuccessStatusCode, $"response code was actually {updateResponse.StatusCode}");
+            CheckContentType(updateResponse);
+
+            var updateContent = await DeserializeObject<WordModel>(updateResponse);
+
+            Assert.NotNull(updateContent);
+            Assert.True(updateContent.WordString.Equals(content.WordString));
+
+            //Delete
+            var deleteResponse = await client.DeleteAsync($"api/word/{updateContent.WordId}");
+            Assert.NotNull(deleteResponse);
+            Assert.True(deleteResponse.IsSuccessStatusCode, $"response code was actually {deleteResponse.StatusCode}");
+        }
+
 
         private void CheckContentType(HttpResponseMessage response) {
             Assert.True(response.Content.Headers.ContentType.MediaType == "application/json");
@@ -100,5 +149,18 @@ namespace HangmanAPI.Test {
             Assert.NotNull(content);
             return content;
         }
+
+        private async Task<Guid> getGameForTesting() {
+            var gameIdResponse = await client.PostAsync("/api/game", null);
+
+            using var streamReader = new StreamReader(await gameIdResponse.Content.ReadAsStreamAsync());
+            using var jsonReader = new JsonTextReader(streamReader);
+
+            var gameId = serializer.Deserialize<Guid>(jsonReader);
+            return gameId;
+        }
+
+        private static StringContent GetStringContent(object obj)
+            => new StringContent(JsonConvert.SerializeObject(obj), Encoding.Default, "application/json");
     }
 }
